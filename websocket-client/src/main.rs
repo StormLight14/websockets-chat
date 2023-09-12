cf449dee6ea31e935a9e789aec829986bfad3dad
Storm <stormpower44@protonmail.com>
2023-09-12 15:58:27 -0600

    add the code stuffs

---
+++ websocket-client/src/main.rs
@@ -0,0 +1,56 @@
+use std::env;
+
+use futures_util::{future, pin_mut, StreamExt};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
+
+// set user with cargo run (user)
+
+#[tokio::main]
+async fn main() {
+    let connect_addr = "ws://127.0.0.1:8080";
+    let username = env::args()
+        .nth(1)
+        .unwrap_or_else(|| panic!("this program requires at least one argument."));
+
+    let url = url::Url::parse(&connect_addr).unwrap();
+
+    let (stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded();
+    tokio::spawn(read_stdin(username, stdin_tx));
+
+    let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
+    println!("WebSocket handshake has been successfully completed");
+
+    let (write, read) = ws_stream.split();
+
+    let stdin_to_ws = stdin_rx.map(Ok).forward(write);
+    let ws_to_stdout = {
+        read.for_each(|message| async {
+            let data = message.unwrap().into_data();
+            tokio::io::stdout().write_all(&data).await.unwrap();
+        })
+    };
+
+    pin_mut!(stdin_to_ws, ws_to_stdout);
+    future::select(stdin_to_ws, ws_to_stdout).await;
+}
+
+// Our helper method which will read data from stdin and send it along the
+// sender provided.
+async fn read_stdin(username: String, tx: futures_channel::mpsc::UnboundedSender<Message>) {
+    let mut stdin = tokio::io::stdin();
+    loop {
+        let mut buf = [0; 1024];
+        let n = match stdin.read(&mut buf).await {
+            Err(_) | Ok(0) => break,
+            Ok(n) => n,
+        };
+
+        let str_buf = std::str::from_utf8(&buf[0..n]).unwrap(); // Convert bytes to a string
+        let formatted_str = format!("{}: {}", username, str_buf);
+
+        // Now, convert the formatted string back to bytes and send it
+        tx.unbounded_send(Message::binary(formatted_str.as_bytes().to_vec()))
+            .unwrap();
+    }
+}
